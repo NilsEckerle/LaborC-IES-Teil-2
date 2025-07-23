0b0a5f613de5dac41b71c30b9b488865d6a85b4f
ies <ies@ies-labor>
2025-07-23 21:08:57 +0200

    Add DTO management system to roboter
    - Add vp_dto field to t_roboter struct
    - Add ROBOTER_set_dto macro for type-safe data transfer
    - Initialize vp_dto to NULL in ROBOTER_init

--- include/roboter/roboter.h
+++ include/roboter/roboter.h
@@ -10,9 +10,21 @@ typedef struct roboter {
 	uint8_t ui8_LF_ADC_avg_samples;
 
 	int8_t i8_rounds;
+	void *vp_dto;
 } t_roboter;
 
 void ROBOTER_init(t_roboter *robi);
 t_roboter *ROBOTER_get_instance();
+#define ROBOTER_set_dto(t_data) do { \
+	t_roboter *tp_robi = ROBOTER_get_instance(); \
+	if (tp_robi->vp_dto != NULL) { \
+		free(tp_robi->vp_dto); \
+		tp_robi->vp_dto = NULL; \
+	} \
+	void *vp_data = malloc(sizeof(typeof(t_data))); \
+	typeof(t_data) t_data_copy = (t_data); \
+	memcpy(vp_data, &t_data_copy, sizeof(typeof(t_data_copy))); \
+	tp_robi = vp_data; \
+} while (0)
 
 #endif // !ROBOTER_H
