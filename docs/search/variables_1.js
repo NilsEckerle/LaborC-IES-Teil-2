9938fa60d695e1ad250c316a9122819645fd0968
ies <ies@ies-labor>
2025-07-29 10:38:36 +0200

    deleted docs

--- docs/search/variables_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['bool_5fis_5fnew_5fstate_0',['bool_is_new_state',['../d2/d83/structstate__machine.html#ad5eeb77522ad86ec249a26ea6ff14495',1,'state_machine']]]
-];
--- docs/search/variables_9.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['seconds_0',['seconds',['../dc/d54/clock_8c.html#a2feed72a43142cb83b74ce2b084ec576',1,'clock.c']]],
-  ['state_1',['state',['../d3/d3a/structedge.html#a3efd126dced2a1c7320e6efa188a9433',1,'edge']]]
-];
